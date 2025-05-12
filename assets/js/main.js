d00b7c3efdb7770c958bc8a5f46d8d4e2f0cbf6c
fabianosses <fabian.osses@gmail.com>
2025-05-12 02:41:15 +0000

    agrega link hacia redes sociales y enlace hacia titulos del mismo documento

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+
+
+$('.btn').click(function(){
+    alert("datos enviados correctamente")
+})
